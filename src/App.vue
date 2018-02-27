c14021ab3149aa72516047fc363773c5f581c260
Mix Jiang <mixitaliano@gmail.com>
2018-02-27 11:00:26 +0800

    upload files

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/gingerlogos.png">
+    <HelloWorld/>
+    <firstcomponent></firstcomponent>
+  </div>
+
+</template>
+
+<script>
+import firstcomponent from './components/firstcomponent.vue'
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  components: { firstcomponent }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/firstcomponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="firstcomponent">
+    <h1>G I N G E R W O R K S</h1>
+    <a>founded by {{ founder }} </a>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        founder: "mix"
+      }
+    }
+  }
+</script>
